19dfb37a19623c261481283d61be400b1f88a7d1
ritatechie <rita.nwaodu@gmail.com>
2022-03-17 09:10:53 -0700

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_triangle - display a triangle
+ * @size:integer
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i, j x;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (i = 0; i < size; i++)
+	{
+		for (j = size - i; j > 1; j--)
+			_putchar(' ');
+		for (x = i + j; x >= 1; x--)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - fizz buzz
+ * Return: void
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (((i % 5) == 0) && ((i % 3) == 0))
+		{
+		printf("FizzBuzz");
+		}
+		else if ((i % 5) == 0)
+		{
+		printf("Buzz");
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("Fizz");
+		}
+		else
+		{
+			printf("%i", i);
+		}
+		if (i < 100)
+		printf(" ");
+	}
+printf("\n");
+return (0);
+}
