e836246d9bd9b215ba55de9f083e66f0d64a6931
ritatechie <rita.nwaodu@gmail.com>
2022-03-17 00:20:27 -0700

    print

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 numbers
+ * @a: integer
+ * @b: integer
+ * Return: Success
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - displays number 0 to 9 followed by new line
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
