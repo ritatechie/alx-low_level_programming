5302fefecc8c53f35c4c8d0148ca14f229141b81
ritatechie <rita.nwaodu@gmail.com>
2022-04-11 17:28:55 -0700

    new dog corrected

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,32 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE dog.h
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- *  * struct dog - Define a new struct dog with name, age, owner
- *   * @name: name of the dog
- *    * @age: age of the dog
- *     * @owner: owner of dog
- *      **/
+ * struct dog - attributes of a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the dog's owner
+ */
 struct dog
 {
-		char *name;
-			float age;
-				char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif
+
+
+#endif /* DOG_H */
